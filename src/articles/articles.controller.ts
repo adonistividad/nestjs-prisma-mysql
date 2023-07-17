a7152bdfabd9dd4ac69ac22513f4758881d7dcc7
Adonis Tividad <adonis@spls.ae>
2023-07-17 12:06:14 +0400

    first commit

---
+++ src/articles/articles.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'src/prisma-client-exception/prisma-client-exception.filter';
+import { ArticlesService } from './articles.service';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UpdateArticleDto } from './dto/update-article.dto';
+import { ArticleEntity } from './entities/article.entity';
+
+@Controller('articles')
+@ApiTags('articles')
+@UseFilters(PrismaClientExceptionFilter)
+export class ArticlesController {
+  constructor(private readonly articlesService: ArticlesService) {}
+
+  @Post()
+  @ApiCreatedResponse({ type: ArticleEntity })
+  create(@Body() createArticleDto: CreateArticleDto) {
+    return this.articlesService.create(createArticleDto);
+  }
+
+  @Get()
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findAll() {
+    return this.articlesService.findAll();
+  }
+
+  @Get('drafts')
+  @ApiOkResponse({ type: ArticleEntity, isArray: true })
+  findAllDrafts() {
+    return this.articlesService.findAllDrafts();
+  }
+
+  @Get(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  async findOne(@Param('id') id: string) {
+    const article = await this.articlesService.findOne(+id);
+    console.log('article::', article);
+    if (!article) {
+      throw new NotFoundException(`Could not find article with ${id}.`);
+    }
+    return article;
+  }
+
+  @Patch(':id')
+  @ApiCreatedResponse({ type: ArticleEntity })
+  update(@Param('id') id: string, @Body() updateArticleDto: UpdateArticleDto) {
+    return this.articlesService.update(+id, updateArticleDto);
+  }
+
+  @Delete(':id')
+  @ApiOkResponse({ type: ArticleEntity })
+  remove(@Param('id') id: string) {
+    return this.articlesService.remove(+id);
+  }
+}
